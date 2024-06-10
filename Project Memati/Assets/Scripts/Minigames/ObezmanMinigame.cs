f14f7f9788a891f2ae25d254c6f2d3a7413f7b92
FrBsrr <furkanbasar453@gmail.com>
2024-06-11 01:36:51 +0300

    added maze system
    maze spawn and despawn for obezman

--- Project Memati/Assets/Scripts/Minigames/ObezmanMinigame.cs
+++ Project Memati/Assets/Scripts/Minigames/ObezmanMinigame.cs
@@ -1,5 +1,6 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObezmanMinigame : MinigameBase
@@ -10,11 +11,16 @@ public class ObezmanMinigame : MinigameBase
 
     public int score = 0;
 
+    public GameObject[] mazePrefabs;
+    public Transform[] spawnPoints;
 
-    
     public int iks = 5;
     public int ye = 5;
     public float bosluk = 2.0f;
+
+    private List<GameObject> spawnedMazes = new List<GameObject>();
+    private float switchTime = 5.0f;
+    private float moveDuration = 1.0f;
     public override void Init()
     {
     }
@@ -22,6 +28,8 @@ public class ObezmanMinigame : MinigameBase
     private void Start()
     {
         SpawnBurgers();
+        SpawnInitialMaze();
+        StartCoroutine(SwitchMazes());
 
     }
 
@@ -33,10 +41,71 @@ public class ObezmanMinigame : MinigameBase
         {
             for (int j = 0; j < ye; j++)
             {
-                Vector3 spawnPosition = startPosition + new Vector3(i*bosluk, 0, j*bosluk);
+                Vector3 spawnPosition = startPosition + new Vector3(i * bosluk, 0, j * bosluk);
 
                 Instantiate(burger, spawnPosition, Quaternion.identity);
             }
         }
     }
+
+    void SpawnInitialMaze()
+    {
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        int randomIndex = Random.Range(0, mazePrefabs.Length);
+        GameObject maze = Instantiate(mazePrefabs[randomIndex], spawnPoint.position, Quaternion.identity);
+
+        spawnedMazes.Add(maze);
+    }
+    IEnumerator SwitchMazes()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(switchTime);
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            int randomIndex = Random.Range(0, mazePrefabs.Length);
+            GameObject newMaze = Instantiate(mazePrefabs[randomIndex], spawnPoint.position - new Vector3(0, 10, 0), Quaternion.identity);
+            yield return StartCoroutine(MoveMazes(newMaze));
+        }
+    }
+
+    IEnumerator MoveMazes(GameObject newMaze)
+    {
+        float elapsedTime = 0;
+        List<Vector3> startPositions = new List<Vector3>();
+        List<Vector3> endPositions = new List<Vector3>();
+
+        foreach (var labirent in spawnedMazes)
+        {
+            startPositions.Add(labirent.transform.position);
+            endPositions.Add(labirent.transform.position - new Vector3(0, 10, 0));
+        }
+
+        Vector3 newMazeStartPosition = newMaze.transform.position;
+        Vector3 newMazeEndPosition = newMaze.transform.position + new Vector3(0, 10, 0);
+
+        while (elapsedTime < moveDuration)
+        {
+            for (int i = 0; i < spawnedMazes.Count; i++)
+            {
+                spawnedMazes[i].transform.position = Vector3.Lerp(startPositions[i], endPositions[i], elapsedTime / moveDuration);
+            }
+
+            newMaze.transform.position = Vector3.Lerp(newMazeStartPosition, newMazeEndPosition, elapsedTime / moveDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        for (int i = 0; i < spawnedMazes.Count; i++)
+        {
+            spawnedMazes[i].transform.position = endPositions[i];
+            Destroy(spawnedMazes[i]);
+        }
+
+        newMaze.transform.position = newMazeEndPosition;
+        spawnedMazes.Clear();
+        spawnedMazes.Add(newMaze);
+    }
+
+
 }
